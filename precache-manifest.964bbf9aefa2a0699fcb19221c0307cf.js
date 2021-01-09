feed4195a118ebc150662b82054aff52ba28469f
unknown <unblocktechie@gmail.com>
2021-01-09 11:44:14 +0530

    Updates

---
+++ precache-manifest.964bbf9aefa2a0699fcb19221c0307cf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f912019b4c050063fc8c",
+    "url": "/fudigo/static/js/main.f912019b.chunk.js"
+  },
+  {
+    "revision": "0e5961d174806e243897",
+    "url": "/fudigo/static/css/1.7c85b6b2.chunk.css"
+  },
+  {
+    "revision": "0e5961d174806e243897",
+    "url": "/fudigo/static/js/1.0e5961d1.chunk.js"
+  },
+  {
+    "revision": "8a9244e1f61d29a7a0e1",
+    "url": "/fudigo/static/js/runtime~main.8a9244e1.js"
+  },
+  {
+    "revision": "4cee2121c36013527f60f55994b1e679",
+    "url": "/fudigo/static/media/fdg.4cee2121.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/fudigo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fudigo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fudigo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fudigo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fudigo/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/fudigo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fudigo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/fudigo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fudigo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fudigo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fudigo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fudigo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fudigo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fudigo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fudigo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/fudigo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "2d51e62236671cba2060e310b044dca2",
+    "url": "/fudigo/index.html"
+  }
+];
